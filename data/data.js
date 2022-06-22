8b09a97de3c846aabefe86c86a66ca8e2fcfb946
sayak2k1 <sayak2k1@gmail.com>
2022-06-22 23:40:08 +0530

    Link for Resume is changed

--- data/data.js
+++ data/data.js
@@ -95,6 +95,6 @@ const Info = [
     }
 ]
 
-const resumeURL = ''
+const resumeURL = 'https://drive.google.com/drive/folders/1pBx5NCZQHfO03B6_KwW0ZucMvpU2-8tG?usp=sharing'
 
 export { navigations, KeySkills, Interests, resumeURL, Info }
